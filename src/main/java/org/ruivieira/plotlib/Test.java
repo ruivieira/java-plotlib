56b081284e55dbb1f4883ad18b6d061670ca2999
Rui  Vieira <ruidevieira@googlemail.com>
2019-03-07 23:12:46 +0000

    Release 0.0.3

--- src/main/java/org/ruivieira/plotlib/Test.java
+++ src/main/java/org/ruivieira/plotlib/Test.java
@@ -1,5 +1,10 @@
 package org.ruivieira.plotlib;
 
+import org.ruivieira.plotlib.plots.ScatterPlot;
+
+import java.util.Arrays;
+
+
 public class Test {
 
     public static void main(String[] args) {
@@ -9,9 +14,9 @@ public class Test {
         Integer[] x = new Integer[]{1, 2, 3, 4};
         Integer[] y = new Integer[]{7, 9, 5, 6};
 
-//        figure.add(new ScatterPl/ot<>(x, y));
+        figure.add(new ScatterPlot<>(Arrays.asList(x), Arrays.asList(y)));
 
-//        figure.save();
+        figure.getBufferedImage();
 
     }
 }
